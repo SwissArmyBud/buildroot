241779fc5418fbb542539748addb9f3a7f01abec
AndrewP <swissarmybud@gmail.com>
2017-08-15 14:26:48 -0700

    Added fstab entry and config for u-boot utils to function in target once booted

--- board/mbay/rpi3/post-build.sh
+++ board/mbay/rpi3/post-build.sh
@@ -9,3 +9,10 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+# Add automount option for u-boot utils to work on
+mkdir ${TARGET_DIR}/boot
+grep -qE '^/dev/mmcblk0p1' ${TARGET_DIR}/etc/fstab || \
+	sed -i '/dev/mmcblk0p1	/boot	defaults	0	0	0' ${TARGET_DIR}/etc/fstab
+	
+echo "/boot/uboot.env	0x0000	0x4000	0x4000" > ${TARGET_DIR}/etc/fw_env.config
