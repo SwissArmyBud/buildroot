59a2a24f8ead44f3f348b3ed74e443cecb049b3f
AndrewP <swissarmybud@gmail.com>
2017-08-15 23:12:03 -0700

    Fixed .config issue with Buildroot environment not building correctly, changed pathing in post-image.sh while tigtening target routine, and corrected missing directory problem for Linux kernel patch staging area.

--- board/mbay/rpi3/post-image.sh
+++ board/mbay/rpi3/post-image.sh
@@ -5,17 +5,28 @@ BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-cp board/jumpnow/${BOARD_NAME}/config.txt ${BINARIES_DIR}/config.txt
-echo "start_x=1\nenable_uart=1" >> ${BINARIES_DIR}/config.txt
-cp board/jumpnow/${BOARD_NAME}/cmdline.txt ${BINARIES_DIR}/cmdline.txt
+# Copy over the 1st stage boot configuration, set GPU mem while enabling camera and uart
+cp board/mbay/${BOARD_NAME}/config.txt ${BINARIES_DIR}/config.txt
+echo "gpu_mem=256" >> ${BINARIES_DIR}/config.txt
+echo "start_x=1" >> ${BINARIES_DIR}/config.txt
+echo "enable_uart=1" >> ${BINARIES_DIR}/config.txt
+cp board/mbay/${BOARD_NAME}/cmdline.txt ${BINARIES_DIR}/cmdline.txt
 
+# If UBoot exists and has been built, move to binaries directory and enable as 2nd stage kernel
 if [-e ${BUILD_DIR}/uboot-${UBOOT_VERSION}/u-boot.bin]
 	echo "Found uboot binary, moving to output..."
 	cp output/build/uboot-${UBOOT_VERSION}/u-boot.bin ${BINARIES_DIR}/
+	echo "kernel=u-boot.bin" >> ${BINARIES_DIR}/config.txt
 fi
 
-rm -rf "${GENIMAGE_TMP}"
+# If Linux has been freshly built, ensure the kernel is appropriately named for 2nd or 3rd stage boot
+if [-e ${BINARIES_DIR}/zImage]
+	echo "Found raw kernel binary, renaming in output..."
+	mv ${BINARIES_DIR}/zImage ${BINARIES_DIR}/kernel7.img
+fi
 
+# Clear the image building directory and rebuild the new binary system image
+rm -rf "${GENIMAGE_TMP}"
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
