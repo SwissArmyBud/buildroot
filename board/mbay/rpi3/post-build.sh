5e9e86768791c8432372de85ed18816dab4a9073
Andrew Porter <swissarmybud@gmail.com>
2017-09-17 14:11:34 -0700

    Moved SWupdated to new package

--- board/mbay/rpi3/post-build.sh
+++ board/mbay/rpi3/post-build.sh
@@ -16,10 +16,3 @@ mkdir ${TARGET_DIR}/boot
 grep -qE '^/dev/mmcblk0p1' ${TARGET_DIR}/etc/fstab || \
 	echo '/dev/mmcblk0p1	/boot		vfat	defaults	0	0	0' >> ${TARGET_DIR}/etc/fstab
 echo "/boot/uboot.env	0x0000	0x4000	0x4000" > ${TARGET_DIR}/etc/fw_env.config
-
-# CHMOD the SystemV initializer
-chmod +x ${TARGET_DIR}/etc/init.d/S30sysctl
-
-# Copy the udev rule and script for writing SD events to a UNIX SOCKET
-cp board/mbay/${BOARD_NAME}/80autousb.rules ${TARGET_DIR}/etc/udev/rules.d/80autousb.rules
-cp board/mbay/${BOARD_NAME}/socketpinger.sh ${TARGET_DIR}/socketpinger.sh
---
+++ package/swupdated/swupdated.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# swupdated
+#
+################################################################################
+
+SWUPDATED_VERSION = master
+SWUPDATED_SITE = http://github.com/swissarmybud/swupdated
+SWUPDATED_SITE_METHOD = git
+SWUPDATED_INSTALL_TARGET = YES
+SWUPDATED_DEPENDENCIES = swupdate
+
+define SWUPDATED_COPY_RULES_TO_TARGET
+	cp $(BUILD_DIR)/swupdated-$(SWUPDATED_VERSION)/*.rules $(TARGET_DIR)/etc/udev/rules.d/
+endef
+SWUPDATED_POST_INSTALL_TARGET_HOOKS += SWUPDATED_COPY_RULES_TO_TARGET
+
+define SWUPDATED_COPY_SCRIPT_TO_TARGET
+	cp $(BUILD_DIR)/swupdated-$(SWUPDATED_VERSION)/socketclient.sh $(TARGET_DIR)/etc/udev/client.sh
+endef
+SWUPDATED_POST_INSTALL_TARGET_HOOKS += SWUPDATED_COPY_SCRIPT_TO_TARGET
+
+define SWUPDATED_COPY_SERVER_TO_TARGET
+	cat $(BUILD_DIR)/swupdated-$(SWUPDATED_VERSION)/socketserver.js | sed -e 's|//.*||g' -e '/^\s*$/d' > $(TARGET_DIR)/etc/udev/server.js
+	echo "  - FINISHED!"
+endef
+SWUPDATED_POST_INSTALL_TARGET_HOOKS += SWUPDATED_COPY_SERVER_TO_TARGET
+
+define SWUPDATED_INSTALL_TARGET_CMDS
+	echo "  - INSTALLING: Node.js SWupdate Daemon for sysV & udev"
+endef
+
+$(eval $(generic-package))
