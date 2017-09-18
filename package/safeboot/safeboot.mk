0856c9f762b2053c44632f115178a966e0c4a00b
Andrew Porter <swissarmybud@gmail.com>
2017-09-18 00:24:07 -0700

    Pathing tweak in safeboot .mk

--- package/safeboot/safeboot.mk
+++ package/safeboot/safeboot.mk
@@ -8,12 +8,12 @@ SAFEBOOT_VERSION = master
 SAFEBOOT_SITE = http://github.com/swissarmybud/safeboot
 SAFEBOOT_SITE_METHOD = git
 SAFEBOOT_INSTALL_TARGET = YES
-SAFEBOOT_DEPENDENCIES = uboot
+SAFEBOOT_DEPENDENCIES = uboot uboot-tools
 
 define SWUPDATED_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/etc/safeboot
 	mkdir $(TARGET_DIR)/etc/safeboot
-	cp $(BUILD_DIR)/safeboot-$(SAFEBOOT_VERSION)/*/* $(TARGET_DIR)/etc/swupdated/
+	cp $(BUILD_DIR)/safeboot-$(SAFEBOOT_VERSION)/*/* $(TARGET_DIR)/etc/safeboot/
 endef
 
 $(eval $(generic-package))
